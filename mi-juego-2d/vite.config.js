45497b66d74e6f90b3b379b03c9cc2912bccab95
Rodrigo Del Villar <117112695+Villa19d@users.noreply.github.com>
2025-03-24 22:50:14 -0600

    Update vite.config.js

--- mi-juego-2d/vite.config.js
+++ mi-juego-2d/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: '/videojuego2d/' // Asegúrate que coincida con tu repo
 })
